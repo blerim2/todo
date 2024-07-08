e50a5a1c05fa0dc821331b819e546dd03bac373b
blerim2 <blerimballiu19@gmail.com>
2024-07-08 14:28:33 +0200

    class Project

--- app/models.py
+++ app/models.py
@@ -7,3 +7,11 @@ class User(db.Model):
     email = db.Column(db.String(128), unique=True, nullable=False)
     password = db.Column(db.String(128), nullable=False)
     projects = db.relationship("Project", backref="user")
+    
+
+class Project(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(64), nullable=False)
+    description = db.Column(db.String(1024))
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    tasks = db.relationship("Task", backref="project")
