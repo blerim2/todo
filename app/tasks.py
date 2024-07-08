1fad49316fecd6c52d2e7290cf27b259b5706a81
blerim2 <blerimballiu19@gmail.com>
2024-07-08 14:54:31 +0200

    getting the id from the project

--- app/tasks.py
+++ app/tasks.py
@@ -16,3 +16,20 @@ def create_task():
     description = data.get("description", None)
     project_id = data.get("project_id", None)
     user_id = get_jwt_identity()
+    
+    # validate fileds
+    if not title or not project_id:
+        return jsonify({"message": "Title and Project_ID is required!"}), 400
+
+   
+
+    # get all projects generated from user
+    projects = db.session.execute(
+        db.select(Project).filter_by(user_id=user_id)
+    ).scalars()
+
+    # get all ids from projects
+    project_arr = [project.serialize() for project in projects]
+    project_id_all = []
+    for i in range(0, len(project_arr)):
+        project_id_all.append(project_arr[i]["id"])
